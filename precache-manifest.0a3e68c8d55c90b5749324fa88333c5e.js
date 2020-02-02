c7ef7f79e039f9896a47e52b388b3ec341817104
Stefan Vasic <vasicstefan123@gmail.com>
2020-02-02 18:16:12 +0100

    Updates

---
+++ precache-manifest.0a3e68c8d55c90b5749324fa88333c5e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0adb37c494d5c05bbe91b7fcf22feb24",
+    "url": "/uptimedev/index.html"
+  },
+  {
+    "revision": "f7245c04b37a2646c446",
+    "url": "/uptimedev/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "daaf65dfaeaff7a8e98c",
+    "url": "/uptimedev/static/css/main.88dc9295.chunk.css"
+  },
+  {
+    "revision": "f7245c04b37a2646c446",
+    "url": "/uptimedev/static/js/2.7905b53d.chunk.js"
+  },
+  {
+    "revision": "7d127dd731bd76a70844d4cf50e01fd0",
+    "url": "/uptimedev/static/js/2.7905b53d.chunk.js.LICENSE"
+  },
+  {
+    "revision": "daaf65dfaeaff7a8e98c",
+    "url": "/uptimedev/static/js/main.bd5d3cdb.chunk.js"
+  },
+  {
+    "revision": "d9c11f57c9290a965ce3",
+    "url": "/uptimedev/static/js/runtime-main.c9c8f57f.js"
+  }
+]);
